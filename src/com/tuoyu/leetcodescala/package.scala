d372bcc39b064c7ca3387c7b39b2308c8e513aba
tuoyu <zhangtuoyu@gardenllc.com>
2016-06-24 16:06:53 +0800

    init

---
+++ src/com/tuoyu/leetcodescala/package.scala
@@ -0,0 +1,13 @@
+package com.tuoyu.leetcodescala
+
+/**
+ * Created by tuoyu on 6/24/16.
+ */
+
+object TwoSum {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    Array(0, 1)
+  }
+}
+
+
