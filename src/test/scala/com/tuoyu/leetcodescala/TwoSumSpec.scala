68750b016b0280eb871b2bd400ce3a02a5124a58
tuoyu <zhangtuoyu@gardenllc.com>
2016-06-27 13:00:29 +0800

    TwoSumSpec init

---
+++ src/test/scala/com/tuoyu/leetcodescala/TwoSumSpec.scala
@@ -0,0 +1,16 @@
+package com.tuoyu.leetcodescala
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.Matchers
+
+/**
+  * Created by tuoyu on 6/27/16.
+  */
+
+class TwoSumSpec extends FlatSpec with Matchers {
+
+  "TwoSum Spec" should "OK" in {
+
+  }
+}
+
