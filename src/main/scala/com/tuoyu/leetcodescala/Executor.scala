39258e42d3315cd3ce1b61538a0f2877198c7fe6
tuoyu <zhangtuoyu@gardenllc.com>
2016-06-29 17:47:21 +0800

    输出格式调整

--- src/main/scala/com/tuoyu/leetcodescala/Executor.scala
+++ src/main/scala/com/tuoyu/leetcodescala/Executor.scala
@@ -2,6 +2,7 @@ package com.tuoyu.leetcodescala
 
 /**
   * Created by tuoyu on 6/27/16.
+  * Executor是用来做自动执行的trait,每个题目的class继承这个trait
   */
 
 
@@ -10,15 +11,20 @@ trait Executor {
   def name: String = getClass.getSimpleName
   def runImplement(): Unit
   def run(): Unit = {
-    println(s" Start ${name} Test")
+    println(s"  +++++++ Start ${name} Test +++++++")
     val start: Long = System.nanoTime()
+    print("\t")
     (1 to runCount) foreach { i =>
       val x = runImplement()
+      if (i % (runCount / 10) == 0 )
+        print(".")
       x
     }
+    print("\n")
     val end: Long = System.nanoTime()
-    val ns: Long = (end - start) / 1000
-
-    println(s" Run ${name} Test ${runCount} Timers Total Time = ${ns} ms Average Time = ${ns/runCount} ms")
+    val ms: Long = (end - start) / 1000
+    println(s" \tRun Times   : ${runCount}")
+    println(s" \tTotal Time  : ${ms} ms")
+    println(s" \tAverage Time: ${ms/runCount} ms\n")
   }
 }
